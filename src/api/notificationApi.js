fd0f36e384816845b584374148a7515b24541b80
danielOh <139105186+daniel0h02@users.noreply.github.com>
2026-03-25 11:17:49 +0900

    baseUrl update

--- src/api/notificationApi.js
+++ src/api/notificationApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8085/api/v1/notifications";
+const API_BASE_URL = "https://server.don-forget.shop/api/v1/notifications";
 
 export const notificationApi = {
   // 알림 목록 조회
